088fec717860def8e902feec44f72710694e62c3
Mario Garcia <mandres_garcia@hotmail.com>
2023-03-17 15:07:59 -0500

    add new email addresses

--- Modules/functions.py
+++ Modules/functions.py
@@ -9,7 +9,7 @@ def Send_Mail(text, Subject):
     msg.set_content(text)
     msg['Subject'] = Subject
     msg['From'] = 'alarmas-aws@vcmedios.com.co'
-    msg['To'] = ['ingenieriavod@vcmedios.com.co']
+    msg['To'] = ['ingenieriavcmc@vcmedios.com.co', 'Gtecnicovcmc@vcmedios.com.co', 'cparada@vcmedios.com.co']
     conexion = smtplib.SMTP(host='10.10.122.17', port=25)
     conexion.ehlo()
     conexion.send_message(msg)
